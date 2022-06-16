1dd1dd3eb25aac082575e6d63c1734a844d9afad
Pedro Gomes <pedrogomes2000.3@gmail.com>
2022-06-16 14:41:31 +0100

    Added content to the homepage

--- views/index.js
+++ views/index.js
@@ -8,13 +8,52 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>Welcome back, Dom</h1>
-            <p>
-                Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
-            </p>
-            <p>
-                <a href="/posts" data-link>View recent posts</a>.
-            </p>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>Hi, I'm Pedro Bentes</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>About Me</h3>
+                </div>
+                <div class="col-sm-3">
+                    <img style="width: 100%;" src="./assets/38290727.jpg" alt="">
+                </div>
+                <div class="col-sm-9">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ab sunt ex deleniti, debitis sed. Nisi iste perspiciatis praesentium, quod, officiis magni nobis eos, possimus corrupti minus error similique nam consectetur dolorem unde rerum accusamus veritatis optio. Reprehenderit quaerat, sequi id, voluptatum illo ducimus sapiente, facilis architecto molestiae ut odio.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-6">
+                    <h3>Latest Blog Posts</h3>
+                    <ul>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                    </ul>
+                </div>
+                <div class="col-sm-6">
+                    <h3>My Projects</h3>
+                    <ul>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                        <li>Test</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Contact</h3>
+                    <p>You can contact me via my <a href="mailto:pedrogomes2000.3@gmail.com">email</a></p>
+                </div>
+            </div>
         `;
     }
 }
